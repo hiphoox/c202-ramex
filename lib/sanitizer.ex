8245e52b1fd62aa1d3f255d672a5009e9a069d5b
Giezy Alberto Ramirez Rivera <ram.giezy@gmail.com>
2020-03-09 13:44:56 -0600

    Agregamos en el Sanitazer lo que nos permite leer numero de linea separandolos por renglones,etc... By Luis Arturo Sandoval

--- lib/sanitizer.ex
+++ lib/sanitizer.ex
@@ -1,6 +1,9 @@
 defmodule Sanitizer do
+  @spec sanitize_source(binary) :: [binary]
   def sanitize_source(file_content) do
-    trimmed_content = String.trim(file_content)
-    Regex.split(~r/\s+/, trimmed_content)
+    str_list = String.split(file_content,"\n")
+    list = Enum.with_index(str_list)
+    list = Enum.filter(list,fn(x)-> elem(x,0) != "" end)
+    Enum.map(list,fn x -> {String.trim(Regex.replace(~r/^\t+|\n+|\r+/,elem(x,0),"")), elem(x,1) + 1} end)
   end
 end
