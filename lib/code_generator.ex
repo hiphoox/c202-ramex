a2bcec01d90939bdf6ff89b5df56c23e825f7c6f
Gonzalo-Emilio <gonzooooo10@gmail.com>
2020-04-16 15:33:14 -0500

    generador de codigo con operadores unarios

--- lib/code_generator.ex
+++ lib/code_generator.ex
@@ -33,16 +33,70 @@ defmodule CodeGenerator do
     _main:                    ## @main
     """ <>
       code_snippet
+      <>
+      """
+        ret
+      """
   end
 
   def emit_code(:return, code_snippet, _) do
-    """
-        movl    #{code_snippet}, %rax
-        ret
-    """
+    codetoList=String.split(code_snippet, " ")
+    codetoTuple=List.to_tuple(codetoList)
+    const=elem(codetoTuple, 0)
+    #IO.inspect(const)
+    codeSpaces=String.split(code_snippet, "")
+    restList=elem(List.pop_at(codeSpaces, 1), 1)
+    restList2=elem(List.pop_at(restList, 1), 1)
+    rest=List.to_string(restList2)
+    #IO.inspect(rest)
+      """
+          movl    #{const}, %eax
+
+      """ <>
+      rest
   end
 
   def emit_code(:constant, _code_snippet, value) do
-    "$#{value}"
+    "$#{value} "
   end
+
+  def emit_code(:bitwise, code_snippet, _) do
+    codetoList=String.split(code_snippet, " ")
+    codetoTuple=List.to_tuple(codetoList)
+    const=elem(codetoTuple, 0)
+    number =
+      case const do
+        "$" <> rest ->
+          rest
+      end
+
+    if number=="0" do
+      code_snippet <>
+      """
+        movl $1, %eax
+      """
+    else
+      code_snippet <>
+      """
+        movl $0, %eax
+      """
+    end
+
+  end
+
+  def emit_code(:negation, code_snippet, _) do
+    code_snippet <>
+    """
+      neg %eax
+    """
+  end
+
+  def emit_code(:logical_negation, code_snippet, _) do
+    code_snippet <>
+    """
+      not %eax
+    """
+  end
+
+
 end
